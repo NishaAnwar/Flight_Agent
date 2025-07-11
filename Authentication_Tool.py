bd07053b7dbc9cf384ff6567b0efea7c8c72d6a2
NishaAnwar <143272454+NishaAnwar@users.noreply.github.com>
2025-07-11 12:39:24 +0500

    Update Authentication_Tool.py

--- Authentication_Tool.py
+++ Authentication_Tool.py
@@ -4,8 +4,8 @@ def authenticate(_: str) -> str:
     url = "https://bookmesky.com/partner/api/auth/token"
     headers = {"Content-Type": "application/json"}
     payload = {
-        "username": "bookme-sky",
-        "password": "omi@work321"
+        "username": "Enter your username",
+        "password": " password "
     }
     r = requests.post(url, json=payload,headers=headers)
     if r.status_code in [200, 201]:
